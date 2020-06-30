1a5e21aec802d0f74c8d17a366d43682d3668e0e
Quinn94 <mayquinn94@gmail.com>
2020-06-30 08:11:05 +0800

    notes

--- src/hooks/usePlayer.js
+++ src/hooks/usePlayer.js
@@ -10,31 +10,34 @@ export const usePlayer = (props) => {
   });
 
   const rotate = (tetromino, direction) => {
-    //Make the rows become columns (transpose)
+    //1. Make the rows become columns (transpose)
     const rotatedTetris = tetromino.map((_, index) =>
       tetromino.map((col) => col[index])
     );
-    //Reverse each row to get a rotated tetris
-    // console.log(rotatedTetris);
-    // console.log(rotatedTetris.reverse());
+    //2. Reverse each row to get a rotated tetris
+    // with every 'up' button, direction will +1
     if (direction > 0) return rotatedTetris.map((row) => row.reverse());
+    //with collision, direction will be -1
     return rotatedTetris.reverse();
   };
-
+  //3. Fix rotating out of the stage
   const playerRotate = (stage, direction) => {
     const clonedPlayer = JSON.parse(JSON.stringify(player));
     clonedPlayer.tetromino = rotate(clonedPlayer.tetromino, direction);
 
-    //fix rotating out of the stage
-    const pos = clonedPlayer.pos.x;
+    const pos = clonedPlayer.pos.x; //position of the player on the stage
     let offset = 1;
     while (checkCollision(clonedPlayer, stage, { x: 0, y: 0 })) {
-      console.log("check-collision");
+      //while checkCollision is true
+      console.log(offset);
       clonedPlayer.pos.x += offset;
-      //create the back and forth movement check
+      console.log(clonedPlayer.pos.x);
+      //to keep track how many steps we moved right and left
       offset = -(offset + (offset > 0 ? 1 : -1));
       console.log(offset);
+      //if clonedPlayer.pos.x already exited the collision area, will not enter the loop below
       if (offset > clonedPlayer.tetromino[0].length) {
+        console.log("entered to if statement");
         rotate(clonedPlayer.tetromino, -direction);
         clonedPlayer.pos.x = pos;
         console.log(pos);
